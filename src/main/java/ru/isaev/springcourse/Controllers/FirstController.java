7ba3ebb1cf9ec4067bf502150893ec340f358a34
Alan <isaev3658@gmail.com>
2022-04-05 14:33:14 +0600

    #20

--- src/main/java/ru/isaev/springcourse/Controllers/FirstController.java
+++ src/main/java/ru/isaev/springcourse/Controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.isaev.springcourse.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,10 +12,10 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage(HttpServletRequest request){
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
-        System.out.println(name + " " + surname);
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model){
+        model.addAttribute("message", name + " " + surname);
 
         return "Pages/hello";
     }
@@ -24,5 +25,30 @@ public class FirstController {
                               @RequestParam(value = "surname", required = false) String surname){
         System.out.println(name + " " + surname);
         return "Pages/goodbye";
+    }
+
+    @GetMapping("/calc")
+    public String calcPage(@RequestParam("a") int a,
+                           @RequestParam("b") int b,
+                           @RequestParam("action") String action,
+                           Model model){
+        double res = 0;
+        switch (action){
+            case "mult":
+                res = a * b;
+                break;
+            case "add":
+                res = a + b;
+                break;
+            case "sub":
+                res = a - b;
+                break;
+            case "div":
+                res = a / b;
+                break;
+        }
+        model.addAttribute("val", res);
+
+        return "Pages/calc";
     }
 }
