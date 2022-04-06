73ee07c0d281c176451fafbd075e504d9249d11f
Alan <isaev3658@gmail.com>
2022-04-06 16:03:09 +0600

    CRUD, DELETE, PATCH

--- src/main/java/ru/isaev/springcourse/DAO/PersonDAO.java
+++ src/main/java/ru/isaev/springcourse/DAO/PersonDAO.java
@@ -26,4 +26,20 @@ public class PersonDAO {
     public Person show(int id){
         return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
     }
+
+    public void save(Person person){
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+
+    public void update(int id, Person person){
+        Person personToBeUpdated = show(id);
+        personToBeUpdated.setName(person.getName());
+    }
+
+    public void delete(int id){
+        /*Person personForRemove = show(id);
+        people.remove(personForRemove);*/
+        people.removeIf(x -> x.getId() == id);
+    }
 }
